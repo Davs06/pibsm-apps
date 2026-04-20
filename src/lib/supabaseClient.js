64a3fad98361683d06e3cff31954a16e53cd1134
Davs06 <santosdv40@gmail.com>
2026-04-20 16:02:46 -0300

    adjust toml

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,4 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("Ambiente:", import.meta.env.MODE);
+console.log("URL do Supabase configurada:", import.meta.env.VITE_SUPABASE_URL ? "Sim" : "Não");
+
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
